4f47610db35ca5bfa5e19f40b3a01cee59c0abd1
Mickael-AY <mickaelayilan@hotmail.com>
2026-02-16 09:49:49 +0100

    Backend & Frontend StarterKit

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+// layouts/MainLayout.jsx
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+
+function MainLayout() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default MainLayout;
